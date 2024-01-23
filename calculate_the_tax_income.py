c63bc5a96d561dc34aec6ae7fab7c3aba384dfdf
Jarvis <edwardmaverickcborboran@iskolarngbayan.pup.edu.ph>
2024-01-23 18:05:47 +0800

    The program seems to be working fine however there is something wrong on the calculation where the final answer should be 6000.0 instead of 3500.0

--- calculate_the_tax_income.py
+++ calculate_the_tax_income.py
@@ -15,7 +15,19 @@
 
 # pseudocode
 
-# Setting up variables for the given income and tax pay
+# Setting up variable for the given income
 given_income = 45000
-# The tax payable starts at 0
-tax_pay = 0
+
+# Use if else statement to calculate the tax income
+if given_income >= 10000:
+    need_tax = given_income - 10000
+    ten_tax = need_tax*0.1
+    print("Your current balance is:", ten_tax)
+elif given_income >= 20000:
+    need_tax_2 = given_income - 20000
+    twenty_tax = need_tax_2*0.2
+    total_twenty_tax =  twenty_tax + 1000
+    print("Your current balance is:", total_twenty_tax)
+else:
+    given_income < 10000
+    print("No need for the tax income.")
