ffe1beb6bc6ab297536e24986c3efaa5518955a8
Jarvis <edwardmaverickcborboran@iskolarngbayan.pup.edu.ph>
2024-01-23 17:23:06 +0800

    Sets some variables that was given.

---
+++ calculate_the_tax_income.py
@@ -0,0 +1,21 @@
+# Create a program that will --
+
+# Calculate the income tax for the given income by adhering to the rules below
+
+# Taxable Income	Rate (in %)
+# First $10,000	    0
+# Next $10,000	    10
+# The remaining	    20
+
+# Expected results: 
+# For example, suppose the taxable income is 45000, and the income tax payable is
+# 10000*0% + 10000*10%  + 25000*20% = $6000.
+
+# DEADLINE: JANUARY 26, 2024
+
+# pseudocode
+
+# Setting up variables for the given income and tax pay
+given_income = 45000
+# The tax payable starts at 0
+tax_pay = 0
